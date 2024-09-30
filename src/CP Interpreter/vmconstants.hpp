ee9dc0eae908331f1b39339ffe3c61daf07ce885
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2024-09-30 00:12:43 -0300

    more commands

--- src/CP Interpreter/vmconstants.hpp
+++ src/CP Interpreter/vmconstants.hpp
@@ -2,43 +2,50 @@
 #define VMCONSTANTS_HPP
 
 enum OpCode : short {
-	//OP_PUSH,
-	//OP_LOAD, // LOAD FROM ADDRESS (VARIABLES, CONSTANTS)
-	//OP_ASSGN,
-	//OP_JSR, // JUMP TO SUBROUTINE
-	//OP_JMPF, // JUMP IF FALSE
-	//OP_JMPT, // JUMP IF TRUE
-	//OP_JMP, // UNCONDITIONAM JUMP
-	//OP_RET, // RETURN FROM SUBROUTINE
-	//OP_EXIT,
-	//OP_TRYS,
-	//OP_TRYE,
-	//OP_THROW
 	OP_RES,
-	OP_LOAD_BOOL_CONST,  // Carrega uma constante na pilha
-	OP_LOAD_INT_CONST,  // Carrega uma constante na pilha
-	OP_LOAD_FLOAT_CONST,  // Carrega uma constante na pilha
-	OP_LOAD_CHAR_CONST,  // Carrega uma constante na pilha
-	OP_LOAD_STRING_CONST,  // Carrega uma constante na pilha
-	OP_LOAD_VAR,    // Carrega uma variável na pilha
-	OP_STORE_VAR,   // Armazena o valor do topo da pilha em uma variável
-	OP_ADD,         // Soma os dois valores no topo da pilha
-	OP_SUB,         // Subtrai os dois valores no topo da pilha
-	OP_MUL,         // Multiplica os dois valores no topo da pilha
-	OP_DIV,         // Divide os dois valores no topo da pilha
-	OP_PRINT,       // Imprime o valor do topo da pilha
-	OP_JUMP,        // Salta para uma instrução
-	OP_JUMP_IF_FALSE, // Salta se o topo da pilha for falso
-	OP_JUMP_IF_TRUE,  // Salta se o topo da pilha for TRUE
-	OP_CALL,          // Chama uma função
-	OP_RETURN,        // Retorna de uma função
+	OP_PUSH_BOOL,
+	OP_PUSH_INT,
+	OP_PUSH_FLOAT,
+	OP_PUSH_CHAR,
+	OP_PUSH_STRING,
+	OP_LOAD_VAR,
+	OP_STORE_VAR,
+	OP_OR,
+	OP_AND,
+	OP_BIT_OR,
+	OP_BIT_XOR,
+	OP_BIT_AND,
+	OP_EQL,
+	OP_DIF,
+	OP_LT,
+	OP_LTE,
+	OP_GT,
+	OP_GTE,
+	OP_SPS,
+	OP_LEFT_SHIFT,
+	OP_RIGHT_SHIFT,
+	OP_ADD,
+	OP_SUB,
+	OP_MUL,
+	OP_DIV,
+	OP_REM,
+	OP_FLD,
+	OP_NOT,
+	OP_BIT_NOT,
+	OP_EXP,
+	OP_JUMP,
+	OP_JUMP_IF_FALSE,
+	OP_JUMP_IF_TRUE,
+	OP_CALL,
+	OP_RETURN,
 	OP_FUN_START,
 	OP_FUN_END,
 	OP_TRY_START,
 	OP_TRY_END,
 	OP_THROW,
+	OP_TRAP,
 	OP_EXIT,
-	OP_HALT         // Finaliza a execução
+	OP_HALT
 };
 
 #endif // !VMCONSTANTS_HPP
